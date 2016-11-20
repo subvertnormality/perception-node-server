293f07a2f39bb7b7471b341b423f5267c5292c09
subvertnormality <andy@subvertnormality.com>
2016-11-20 19:24:35 +0000

    Adding twitch logging and security to websocket

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 const express = require('express');
+const expressSession = require('express-session');
+const expressCookieParser = require('cookie-parser');
+const RedisStore = require('connect-redis')(expressSession);
 const app = express();
 const http = require('http').Server(app);
-const grpc = require('grpc');
-const control = grpc.load(__dirname + '/protos/control.proto').control;
-const rpcStub = new control.Control('1.tcp.eu.ngrok.io:20187', grpc.credentials.createInsecure());
+const io = require('socket.io')(http);
 const initIo = require('./lib/io');
+const routes = require('./lib/routes');
+const passport = require('./lib/passport');
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+const STORE_SECRET = 'Fjnewvi!£wei2847!jfaefb38DJFB09W';
+
+const redisStore = new RedisStore({
+  host: 'redis',
+  port: '6379'
 });
 
-app.get('/currentCozmoImage', function (req, res) {
-
-    rpcStub.handleImageGetEvent({}, function(error, img) {
-        res.end(img.image, 'binary');
-    });
-
-});
+const sessionSettings = {
+    cookieParser: expressCookieParser,
+    key:         'connect.sid',
+    secret:      STORE_SECRET,
+    store:       redisStore
+};
 
 app.use(express.static('public'))
+app.use(expressCookieParser());
+app.use(expressSession(sessionSettings));
+app.use(passport.initialize());
+app.use(passport.session());
 
-initIo(rpcStub, http);
+routes(app, passport);
+
+initIo(io, sessionSettings);
 
 http.listen(3000, function () {
   console.log('Listening on port 3000')
