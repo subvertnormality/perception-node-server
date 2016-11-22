f36941115c78ce6555fdcf2c9ef76e72eaddce80
subvertnormality <andy@subvertnormality.com>
2016-11-22 23:07:20 +0000

    Formatting

--- util/local-ip.js
+++ util/local-ip.js
@@ -1,17 +1,17 @@
-    
+
 var os = require('os');
 var util = require('util');
 var ifaces = os.networkInterfaces();
 
 for (var dev in ifaces) {
-    if (ifaces.hasOwnProperty(dev)) {
-        for (var i = 0, len = ifaces[dev].length; i < len; i++) {
-            var details = ifaces[dev][i];
-            if (details.family === 'IPv4') {
-                console.log(details.address);
-                break;
-            }
-        }
+  if (ifaces.hasOwnProperty(dev)) {
+    for (var i = 0, len = ifaces[dev].length; i < len; i++) {
+      var details = ifaces[dev][i];
+      if (details.family === 'IPv4') {
+        console.log(details.address);
         break;
+      }
     }
+    break;
+  }
 }
