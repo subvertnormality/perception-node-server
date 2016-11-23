599960e1bdcd3e072caa4dc0d996dcd9f38e2fe5
subvertnormality <andy@subvertnormality.com>
2016-11-23 23:26:46 +0000

    Added first unit test of many

--- test/lib/passport-test.js
+++ test/lib/passport-test.js
@@ -1,42 +1,68 @@
 'use strict';
 
 const appRoot = require('app-root-path');
+const chai = require('chai');
 const sinon = require('sinon');
-const expect = sinon.expect;
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
 const rewire = require('rewire');
 const passport = rewire(appRoot + '/lib/passport');
+const db = require(appRoot + '/lib/db');
 const _ = require('lodash');
+
+chai.use(sinonChai);
 
 describe('Passport authentication', () => {
   const resetRewires = [];
-  let userMock;
+  let user;
+  let userFindStub;
+  let userSaveSpy;
   let dbMock;
+  let profileMock;
+  let passportCallback;
 
   before(() => {
-
     dbMock = {
       factory: sinon.stub()
     };
 
-    userMock = {
-      find: sinon.stub()
-    };
-    
-    dbMock.factory.withArgs('User').returns(userMock);
+    user = db.factory('User');
 
+    profileMock = {
+      _json: {
+        name: 'test_name',
+        display_name: 'test_display_name',
+        email: 'test_email',
+        logo: 'test_logo'
+      }
+    }
+    
+    userFindStub = sinon.stub(user, 'find');
+    userSaveSpy = sinon.spy(user, 'save');
+    dbMock.factory.returns(user);
+    
     resetRewires.push(passport.__set__('db', dbMock));
+    passportCallback = (passport.__get__('passportCallback'));
 
   });
 
   after(() => {
     _.each(resetRewires, _.attempt);
+    user.save.restore();
   });
 
   describe('passportCallback', () => {
 
     it('when finding user returns error, updates blank user with twitch info and save', () => {
 
+      let doneSpy = sinon.spy();
 
+      userFindStub.withArgs({name: 'test_name'}).yields(new Error('error'));
+
+      passportCallback('', '', profileMock, doneSpy);
+
+      expect(userSaveSpy).to.have.been.called;
+      expect(doneSpy).to.have.been.calledWith(null, user);
 
     });
 
