3465bac1e379b2d9ae2e6439fdd300d095140eb7
subvertnormality <andy@subvertnormality.com>
2016-12-23 00:52:07 +0000

    Added in game logic UI

--- test/lib/game-test.js
+++ test/lib/game-test.js
@@ -41,20 +41,24 @@ describe('Game', () => {
   });
 
 
-  it('generates game cube location with each cube appearing once', () => {
+  it('generates game cube location with each cube appearing once', (done) => {
 
-    const cubeLocation = game.generateNewRound();
+    game.generateNewRound((err, round) => {
+      const cubeLocation = round.configuration;
 
-    expect(cubeLocation.length).to.equal(6);
-    _.remove(cubeLocation, (loc) => loc === 1);
-    expect(cubeLocation.length).to.equal(5);
-    _.remove(cubeLocation, (loc) => loc === 2);
-    expect(cubeLocation.length).to.equal(4);
-    _.remove(cubeLocation, (loc) => loc === 3);
-    expect(cubeLocation.length).to.equal(3);
-    expect(cubeLocation[0]).to.equal(0);
-    expect(cubeLocation[1]).to.equal(0);
-    expect(cubeLocation[2]).to.equal(0);
+      expect(cubeLocation.length).to.equal(6);
+      _.remove(cubeLocation, (loc) => loc === 1);
+      expect(cubeLocation.length).to.equal(5);
+      _.remove(cubeLocation, (loc) => loc === 2);
+      expect(cubeLocation.length).to.equal(4);
+      _.remove(cubeLocation, (loc) => loc === 3);
+      expect(cubeLocation.length).to.equal(3);
+      expect(cubeLocation[0]).to.equal(0);
+      expect(cubeLocation[1]).to.equal(0);
+      expect(cubeLocation[2]).to.equal(0);
+      done();
+    });
+
  
   });
 
@@ -63,7 +67,7 @@ describe('Game', () => {
     game.startNewRound(() => {
 
       game.currentGame((err, g) => {
-        expect(g.p('round')).to.not.deep.equal([1, 2, 3, 0, 0, 0]);
+        expect(g.p('round').configuration).to.not.deep.equal([1, 2, 3, 0, 0, 0]);
         done();
       });
 
@@ -75,7 +79,7 @@ describe('Game', () => {
 
     game.startNewRound(() => {
       game.currentGame((err, g) => {
-        game.checkWin(g.p('round'), (err, win) => {
+        game.checkWin(g.p('round').configuration, (err, win) => {
           expect(win).to.be.true;
           done();
         });
@@ -89,7 +93,7 @@ describe('Game', () => {
 
     game.startNewRound(() => {
       game.currentGame((err, g) => {
-        const round = g.p('round');
+        const round = g.p('round').configuration;
         game.winRound((err) => {
           game.currentGame((err, g2) => {
             expect(g2.p('score')).to.equal(1);
