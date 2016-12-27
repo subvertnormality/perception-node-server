b93889b599362644f0876b2366a16bc9e93c3934
subvertnormality <andy@subvertnormality.com>
2016-12-27 23:39:02 +0000

    Adding unit tests for game and change space logic

--- test/lib/game-test.js
+++ test/lib/game-test.js
@@ -15,8 +15,12 @@ chai.use(sinonChai);
 
 describe('Game', () => {
   const resetRewires = [];
+  let shuffleStub;
 
   before((done) => {
+    _.shuffle = _.reverse;
+    _.random = _.constant(0);
+    resetRewires.push(game.__set__('_', _));
     redis.flushdb(() => {
       game.currentGame(done);
     });
@@ -67,7 +71,11 @@ describe('Game', () => {
     game.startNewRound(() => {
 
       game.currentGame((err, g) => {
-        expect(g.p('round').configuration).to.not.deep.equal([1, 2, 3, 0, 0, 0]);
+        expect(g.p('round').level).to.equal(0);
+        expect(g.p('round').configuration).to.deep.equal([0, 0, 0, 3, 2, 1]);
+        expect(g.p('round').data.name).to.deep.equal('major');
+        expect(g.p('round').data.complete).to.be.false;
+        expect(g.p('round').data.clue).to.equal('_ a j o r');
         done();
       });
 
@@ -104,5 +112,271 @@ describe('Game', () => {
       });
     });
 
+  });
+
+  it('round 0 is still active at score 49', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 49);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(0);
+            done();
+          });
+        })
+      });
+    });
+
+  });
+
+  it('round 1 begins at score 50', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 50);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(1);
+            done();
+          });
+        })
+      });
+    });
+
+  });
+
+  it('round 1 still at score 99', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 99);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(1);
+            done();
+          });
+        })
+      });
+    });
+
+  });
+
+  it('round 2 begins at score 100', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 100);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(2);
+            done();
+          });
+        })
+      });
+    });
+
+  });
+
+  it('round 61 at score 3098', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 3098);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(61);
+            done();
+          });
+        })
+      });
+    });
+
+  });
+
+  it('round clue has one space at level 1', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 52);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(1);
+            expect(round.data.name).to.deep.equal('coach');
+            expect(round.data.clue).to.equal('_ _ a c h');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has two spaces at level 4', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 102);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(2);
+            expect(round.data.name).to.deep.equal('brand');
+            expect(round.data.clue).to.equal('_ _ a n d');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+
+  it('round clue has three spaces at level 5', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 252);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(5);
+            expect(round.data.name).to.deep.equal('clock');
+            expect(round.data.clue).to.equal('_ _ _ c k');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has three spaces at level 9', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 499);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(9);
+            expect(round.data.name).to.deep.equal('module');
+            expect(round.data.clue).to.equal('_ _ _ u l e');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has four spaces at level 10', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 501);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(10);
+            expect(round.data.name).to.deep.equal('rugby');
+            expect(round.data.clue).to.equal('_ _ _ _ y');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has five spaces at level 20', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 1001);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(20);
+            expect(round.data.name).to.deep.equal('compulsive');
+            expect(round.data.clue).to.equal('_ _ _ _ _ l s i v e');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has six spaces at level 40', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 2001);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(40);
+            expect(round.data.name).to.deep.equal('cookout');
+            expect(round.data.clue).to.equal('_ _ _ _ _ _ t');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has seven spaces at level 60', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 3001);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(60);
+            expect(round.data.name).to.deep.equal('groundfish');
+            expect(round.data.clue).to.equal('_ _ _ _ _ _ _ i s h');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has eight spaces at level 80', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 4001);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(80);
+            expect(round.data.name).to.deep.equal('sinologist');
+            expect(round.data.clue).to.equal('_ _ _ _ _ _ _ _ s t');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('round clue has nine spaces at level 103', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 5150);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(103);
+            expect(round.data.name).to.deep.equal('lowlander');
+            expect(round.data.clue).to.equal('_ _ _ _ _ _ _ _ _');
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it('handles extremely large levels out of clue json bounds', (done) => {
+
+    game.startNewRound(() => {
+      game.currentGame((err, g) => {
+        g.p('score', 55150);
+        g.save((err) => {
+          game.generateNewRound((err, round) => {
+            expect(round.level).to.equal(1103);
+            done();
+          });
+        })
+      });
+    });
   });
 });
